e848cf7cec8464c38e6096ed2c7958726f2fb274
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2019-10-05 23:32:19 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::HokkaidoOkuyami - It's new $module
+
+# SYNOPSIS
+
+    use App::HokkaidoOkuyami;
+
+# DESCRIPTION
+
+App::HokkaidoOkuyami is ...
+
+# LICENSE
+
+Copyright (C) OGATA Tetsuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+OGATA Tetsuji <tetsuji.ogata@gmail.com>
---
+++ lib/App/HokkaidoOkuyami.pm
@@ -0,0 +1,39 @@
+package App::HokkaidoOkuyami;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::HokkaidoOkuyami - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::HokkaidoOkuyami;
+
+=head1 DESCRIPTION
+
+App::HokkaidoOkuyami is ...
+
+=head1 LICENSE
+
+Copyright (C) OGATA Tetsuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+OGATA Tetsuji E<lt>tetsuji.ogata@gmail.comE<gt>
+
+=cut
+
